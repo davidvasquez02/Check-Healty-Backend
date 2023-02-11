16a6dacee3e174d831582a9213d9c010e2dc2166
Jefrey Steven <jefrey2190049@correo.uis.edu.co>
2023-02-11 16:06:24 -0500

    feat Crud Usuario

--- src/main/java/com/software/checkhealtybackend/service/interfaces/IUsuarioService.java
+++ src/main/java/com/software/checkhealtybackend/service/interfaces/IUsuarioService.java
@@ -1,7 +1,17 @@
 package com.software.checkhealtybackend.service.interfaces;
 
+
+import com.software.checkhealtybackend.model.Usuario;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface IUsuarioService {
+
+    Usuario findById (Long aId);
+
+    Usuario createUsuario(Usuario aUsuario);
+
+    Usuario updateUsuario(Usuario aUsuario);
+
+    void deleteUsuario(Long aId);
 }
