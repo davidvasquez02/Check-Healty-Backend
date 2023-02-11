f30003858affd53857f15255f114cdc5b062a350
David Vasquez <davidvasquez_02@outlook.es>
2023-02-11 13:08:41 -0500

    commit inicial - se crea proyecto backend

---
+++ src/test/java/com/software/checkhealtybackend/CheckHealtyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.checkhealtybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckHealtyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
